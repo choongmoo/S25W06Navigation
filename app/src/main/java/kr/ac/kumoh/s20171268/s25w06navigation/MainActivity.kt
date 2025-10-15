c77b6164ccd888320fd833c4022e68c13fd51c5b
choongmoo <wdokl88@naver.com>
2025-10-15 15:21:54 +0900

    🌐Drawer 화면 이동

--- app/src/main/java/kr/ac/kumoh/s20171268/s25w06navigation/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20171268/s25w06navigation/MainActivity.kt
@@ -8,15 +8,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kr.ac.kumoh.s20171268.s25w06navigation.Destinations.SINGER_SCREEN
 import kr.ac.kumoh.s20171268.s25w06navigation.Destinations.SONG_SCREEN
 import kr.ac.kumoh.s20171268.s25w06navigation.ui.theme.S25W06NavigationTheme
@@ -53,22 +63,35 @@ private fun navigateAndClearStack(
 
 @Composable
 fun MainScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        val navController = rememberNavController()
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    // NOTE: Scaffold 안에 있는 navController 주석 처리할 것
+    val navController = rememberNavController()
+    val scope = rememberCoroutineScope()
 
-        NavHost(
-            navController = navController,
-            startDestination = SONG_SCREEN,
-            modifier = Modifier.padding(innerPadding),
-        ) {
-            composable(SONG_SCREEN) {
-                SongScreen {
-                    navigateAndClearStack(navController, SINGER_SCREEN)
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            DrawerSheet(scope, drawerState, navController)
+        },
+        gesturesEnabled = true,
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            // val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = SONG_SCREEN,
+                modifier = Modifier.padding(innerPadding),
+            ) {
+                composable(SONG_SCREEN) {
+                    SongScreen {
+                        navigateAndClearStack(navController, SINGER_SCREEN)
+                    }
                 }
-            }
-            composable(SINGER_SCREEN) {
-                SingerScreen {
-                    navigateAndClearStack(navController, SONG_SCREEN)
+                composable(SINGER_SCREEN) {
+                    SingerScreen {
+                        navigateAndClearStack(navController, SONG_SCREEN)
+                    }
                 }
             }
         }
@@ -96,5 +119,37 @@ fun SingerScreen(onNavigateToSong: () -> Unit) {
         ) {
             Text("노래 화면으로 이동")
         }
+    }
+}
+
+@Composable
+fun DrawerSheet(
+    scope: CoroutineScope,
+    drawerState: DrawerState,
+    navController: NavHostController,
+) {
+    val currentDestination = navController.currentBackStackEntryAsState().value?.destination
+
+    ModalDrawerSheet {
+        NavigationDrawerItem(
+            label = { Text("노래 화면") },
+            selected = currentDestination?.route == SONG_SCREEN,
+            onClick = {
+                scope.launch {
+                    drawerState.close()
+                }
+                navigateAndClearStack(navController, SONG_SCREEN)
+            }
+        )
+        NavigationDrawerItem(
+            label = { Text("가수 화면") },
+            selected = currentDestination?.route == SINGER_SCREEN,
+            onClick = {
+                scope.launch {
+                    drawerState.close()
+                }
+                navigateAndClearStack(navController, SINGER_SCREEN)
+            }
+        )
     }
 }
